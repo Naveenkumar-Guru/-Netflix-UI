b40f4b65599853d0c15115525cbb69c01262b5e4
Naveenkumar506 <naveennani0603@gmail.com>
2025-10-03 21:28:44 +0530

    card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -54,7 +54,7 @@ const Card = () => {
               <div className="info">
                 <BiChevronDown title="more Info" />
               </div>
-              <div className="genre">
+              <div className="genres">
                 <ul>
                   <li>Action</li>
                   <li>Action</li>
@@ -71,6 +71,85 @@ const Card = () => {
 };
 
 const CardContainer = styled.div`
+  max-width: 230px;
+  width: 230px;
+  height: 100%;
+  cursor: pointer;
+  position: relative;
   background-color: red;
+  img {
+    border-radius: 0.2rem;
+    width: 100%;
+    height: 100%;
+    z-index: 10px;
+  }
+  .hover {
+    z-index: 99;
+    height: max-content;
+    width: 20rem;
+    position: absolute;
+    top: -18v;
+    left: 0;
+    border-radius: 0.2rem;
+    border: 0.1rem solid gray;
+    transition: 0ms.3s ease-out;
+    .image-video-wrapper {
+      position: relative;
+      height: 140px;
+      img {
+        width: 100%;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        z-index: 4;
+        position: absolute;
+      }
+      video {
+        width: 100%;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        z-index: 4;
+        position: absolute;
+      }
+      //this i using for icons
+      .icons {
+        display: flex;
+        justify-content: space-between;
+        .controls {
+          display: flex;
+          gap: "0.5rem";
+        }
+
+        svg {
+          color: white;
+          border: 0.1rem solid white;
+          border-radius: 50%;
+          font-size: 1.5rem;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          &:hover {
+            color: #b8b8b8;
+          }
+        }
+      }
+      .genres {
+        display: flex;
+        ul {
+          display: flex;
+          gap: 1rem;
+        }
+        li {
+          padding-right: 0.7rem;
+          &:first-of-type {
+            list-style-type: none;
+          }
+        }
+      }
+    }
+  }
 `;
+
 export default Card;
